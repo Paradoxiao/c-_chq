aa28e153d19c2858737871105d45701b7b0f665a
Paradoxiao <1741841452@qq.com>
2025-05-22 09:50:53 +0800

    n个名字里带A和a的人

--- main.cpp
+++ main.cpp
@@ -1,41 +1,70 @@
+#include <cstring>
 #include <iostream>
 using namespace std;
 class Person {
 private:
-  int age;
-  bool sex;
+  char *name;
 
 public:
   Person() {}
-  void set(int age, bool sex) {
-    this->age = age;
-    this->sex = sex;
+  Person(char *n) {
+    name = new char[strlen(n) + 1];
+    strcpy(name, n);
   }
-  int divide() {
-    if (age <= 20 && sex)
-      return 1;
-    if (age <= 20 && !sex)
-      return -1;
-    return 0;
+  Person(const Person &other) {
+    name = new char[strlen(other.name + 1)];
+    strcpy(name, other.name);
+  }
+  void setName(char *n) {
+    delete[] name;
+    name = new char[strlen(n) + 1];
+    strcpy(name, n);
+  }
+  char *getName() {
+    return name;
+  }
+  ~Person() {
+    delete name;
+  }
+};
+class PersonList {
+private:
+  Person *persons;
+  int n;
+
+public:
+  PersonList(int n) {
+    persons = new Person[n];
+    this->n = n;
+  }
+  void getName() {
+    char name[100];
+    for (int i = 0; i < n; i++) {
+      cin >> name;
+      persons[i].setName(name);
+    }
+  }
+  int count() {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+      char *temp = persons[i].getName();
+      for (int j = 0; j < strlen(temp); j++)
+        if (temp[j] == 'a' || temp[j] == 'A') {
+          count++;
+          break;
+        }
+    }
+    return count;
+  }
+  ~PersonList() {
+    delete[] persons;
   }
 };
 int main() {
   int n;
   cin >> n;
-  int m = 0, w = 0;
-  int age;
-  bool sex;
-  Person persons[n];
-  for (int i = 0; i < n; i++) {
-    cin >> age >> sex;
-    persons[i].set(age, sex);
-  }
-  for (int i = 0; i < n; i++) {
-    if (persons[i].divide() == 1)
-      m++;
-    if (persons[i].divide() == -1)
-      w++;
-  }
-  cout << m << " " << w << endl;
+  PersonList persons(n);
+  persons.getName();
+  cout << persons.count();
   return 0;
 }
