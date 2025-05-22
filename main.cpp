675fb2ce48b678d2a1f35501d42596dd4e68969f
Paradoxiao <1741841452@qq.com>
2025-05-22 20:02:49 +0800

    递归创建链式二叉树

--- main.cpp
+++ main.cpp
@@ -1,26 +1,16 @@
-#include <cstdio>
-#define NULLELEM -1
-struct Sq_BiTree {
-  int *base;
-  int length;
-  int capacity;
+#include <iostream>
+struct node {
+  int data;
+  node *left, *right;
 };
-int GetLeaf(Sq_BiTree T, int root) {
-  if (T.base == NULL || root >= T.length || T.base[root] == NULLELEM)
-    return 0;
-  int left = 2 * root + 1;
-  int right = 2 * root + 2;
-  if ((left >= T.length || T.base[left] == NULLELEM) &&
-      (right >= T.length || T.base[right] == NULLELEM))
-    return 1;
-  int cnt = 0;
-  if (left < T.length)
-    cnt += GetLeaf(T, left);
-  if (right < T.length)
-    cnt += GetLeaf(T, right);
-  return cnt;
-}
-int main() {
-
-  return 0;
+void create(node *tree) {
+  int i;
+  std::cin >> i;
+  tree->data = i;
+  if (i == -1)
+    return;
+  tree->left = new node;
+  tree->right = new node;
+  create(tree->left);
+  create(tree->right);
 }
