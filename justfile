05f7ede59f75e89d6a3945216b7c09cb4098c7f2
Paradoxiao <1741841452@qq.com>
2025-05-22 08:57:28 +0800

    输入n个人统计小于等于20岁的男人和女人

---
+++ justfile
@@ -0,0 +1,3 @@
+default:
+  @g++ -std=c++98 main.cpp -o out.exe
+  @./out.exe
--- main.cpp
+++ main.cpp
@@ -1,33 +1,41 @@
-#include<iostream>
-#include<string>
-#include<cstdio>
+#include <iostream>
 using namespace std;
-class Student
-{
+class Person {
 private:
   int age;
   bool sex;
-  int grad;
+
 public:
-  void eat(string str="orange")
-  {
-    cout<<"I am eating "<<str<<"."<< endl;
+  Person() {}
+  void set(int age, bool sex) {
+    this->age = age;
+    this->sex = sex;
   }
-  void eat(int r=1)
-  {
-    if(r==1)cout<<"I am eating."<<endl;
-    else cout<<"I am not eating."<<endl;
-  }
-  void lesson()
-  {
-    cout<<"I am in a lesson."<< endl;
+  int divide() {
+    if (age <= 20 && sex)
+      return 1;
+    if (age <= 20 && !sex)
+      return -1;
+    return 0;
   }
 };
-int main()
-{
-  Student a;
-  a.eat("apple");
-  a.eat();
-  a.lesson();
+int main() {
+  int n;
+  cin >> n;
+  int m = 0, w = 0;
+  int age;
+  bool sex;
+  Person persons[n];
+  for (int i = 0; i < n; i++) {
+    cin >> age >> sex;
+    persons[i].set(age, sex);
+  }
+  for (int i = 0; i < n; i++) {
+    if (persons[i].divide() == 1)
+      m++;
+    if (persons[i].divide() == -1)
+      w++;
+  }
+  cout << m << " " << w << endl;
+  return 0;
 }
-
