f3c3d874e1ce350e99633d35fbd685d337ac4845
Paradoxiao <1741841452@qq.com>
2025-05-27 09:07:32 +0800

    const和static

--- main.cpp
+++ main.cpp
@@ -1,16 +1,41 @@
+// const
+// 常数数据成员
+// 常成员函数
+// 常对象
+// static
+// 类成员
 #include <iostream>
-struct node {
-  int data;
-  node *left, *right;
+using namespace std;
+class Person {
+private:
+  int age;
+  const bool sex;
+  static int cnt;
+
+public:
+  Person(int a, bool s) : sex(s) {
+    age = a;
+    cnt++;
+  }
+  void print() const {
+    // age = 20;
+    cout << age << " " << sex << endl;
+  }
+  static void setAge(int a) {
+    age = a;
+  }
+  static int getCnt() {
+    return cnt;
+  }
+  // void setSex(bool s) {    sex = s;  }
 };
-void create(node *tree) {
-  int i;
-  std::cin >> i;
-  tree->data = i;
-  if (i == -1)
-    return;
-  tree->left = new node;
-  tree->right = new node;
-  create(tree->left);
-  create(tree->right);
+int Person::cnt = 0;
+int main() {
+  const Person person(18, false);
+  person.setAge(10);
+  cout << Person::getCnt() << " " << person.getCnt() << endl;
+  person.print();
+  // Person.setAge(20);
+  person.print();
+  return 0;
 }
