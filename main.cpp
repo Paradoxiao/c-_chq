752af39f55af76ea7dd691a74ad84593faeb0777
Paradoxiao <1741841452@qq.com>
2025-05-29 09:50:42 +0800

    类的继承权限

--- main.cpp
+++ main.cpp
@@ -1,61 +1,25 @@
-#include <iostream>
-#include <string>
-using namespace std;
 class Base {
-public:
-  Base() {
-    cout << "Base is created.\n";
-  }
-  ~Base() {
-    cout << "Base is erased.\n";
-  }
-};
-class Date : public Base {
-public:
-  Date() {
-    cout << "Date is created.\n";
-  }
-  ~Date() {
-    cout << "Date is erased.\n";
-  }
-};
-class Person {
-protected:
-  string name;
-  int age;
-  Date birthday;
-
-public:
-  Person(string n = "", int a = 0) : name(n), age(a) {
-    cout << "Person is created.\n";
-  }
-  ~Person() {
-    cout << "Person is erased.\n";
-  }
-  void print() {
-    cout << name << " " << age << " ";
-  }
-};
-class Student : public Person {
 private:
-  string major;
-  Date time;
+  int a;
+
+protected:
+  int b;
 
 public:
-  Student(string n = "", int a = 0, string m = "") : Person(n, a), major(m) {
-    cout << "Student is created.\n";
-  }
-  void print() {
-    cout << name << " " << age << " " << major << endl;
-  }
-  ~Student() {
-    cout << "Student is erased.\n";
+  int c;
+};
+class Derived : protected Base {
+public:
+};
+class Derived_Derived : public Derived {
+  void test() {
+    a = 1;
+    b = 2;
+    c = 3;
   }
 };
 int main() {
-  Student Yunru("云茹", 18, "science");
-  Yunru.print();
-  // // Student student;
-  // student.print();
+  Derived_Derived dd;
+  dd.c = 3;
   return 0;
 }
