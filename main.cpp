047573e7953ce3f7c88b7154cd0891ff4b77ab8d
Paradoxiao <1741841452@qq.com>
2025-06-19 09:13:42 +0800

    模板类的特化组合继承

--- main.cpp
+++ main.cpp
@@ -1,22 +1,79 @@
+#include <cstring>
 #include <iostream>
 using namespace std;
-int k = 0;
-void permute(int *a, int n, int l, int r) {
-  if (l == r - 1) {
-    cout << ++k << ":";
-    for (int i = 0; i < n; i++)
-      cout << a[i] << " ";
-    cout << endl;
-    return;
+template <typename T> class Data {
+protected:
+  T mem;
+
+public:
+  Data(T mem = 0) : mem(mem) {}
+  Data<T> &operator=(const Data<T> &other) {
+    mem = other.mem;
+    return *this;
   }
-  for (int i = l; i < r; i++) {
-    swap(a[l], a[i]);
-    permute(a, n, l + 1, r);
-    swap(a[l], a[i]);
+  bool operator>(const Data<T> &other) {
+    return mem > other.mem;
   }
-}
+  friend ostream &operator<<(ostream &os, const Data<T> &other) {
+    os << other.mem;
+    return os;
+  }
+};
+// NOTE: 模板类特化
+template <> class Data<char *> {
+private:
+  char *mem;
+
+public:
+  Data(char *s) : mem(new char[strlen(s) + 1]) {
+    strcpy(mem, s);
+  }
+  ~Data() {
+    delete[] mem;
+  }
+  Data<char *> &operator=(const Data<char *> &other) {
+    delete[] mem;
+    mem = new char[strlen(other.mem) + 1];
+    strcpy(mem, other.mem);
+    return *this;
+  }
+  Data<char *> operator+(const Data<char *> &other) {
+    char *newMem = new char[strlen(mem) + strlen(other.mem) + 1];
+    strcpy(newMem, mem);
+    strcat(newMem, other.mem);
+    Data<char *> result(newMem);
+    delete[] newMem;
+    return result;
+  }
+  bool operator>(const Data<char *> &other) {
+    return strcmp(mem, other.mem) > 0;
+  }
+  friend ostream &operator<<(ostream &os, const Data<char *> &other) {
+    os << other.mem;
+    return os;
+  }
+};
+// NOTE: 模板类组合
+class Student {
+private:
+  Data<char *> name;
+  Data<int> age;
+  Data<double> score;
+
+public:
+  Student(char *n, int a, double s) : name(n), age(a), score(s) {}
+  void print() const {
+    cout << age << " " << score;
+  }
+};
+// NOTE: 模板类继承
+class Data2 : public Data<int> {
+public:
+  Data2(int data) : Data<int>(data) {}
+  void add() {
+    mem++;
+  }
+};
 int main() {
-  int a[] = {1, 2, 3, 4, 5};
-  permute(a, 5, 0, 5);
   return 0;
 }
